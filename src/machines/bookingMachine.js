2a4a52c43eeacc80777c433924c78521b577ed0f
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-07-07 14:34:51 -0500

    add: context at satate machine

--- src/machines/bookingMachine.js
+++ src/machines/bookingMachine.js
@@ -1,32 +1,61 @@
-import { createMachine } from "xstate";
+import { createMachine, assign } from "xstate";
 
-const bookingMachine = createMachine({
-  id: "buy plane tickets",
-  initial: "initial",
-  states: {
-    initial: {
-      on: {
-        START: "search",
-      },
+const bookingMachine = createMachine(
+  {
+    id: "buy plane tickets",
+    initial: "initial",
+    context: {
+      passengers: [],
+      selectedCountry: "",
     },
-    search: {
-      on: {
-        CONTINUE: "passengers",
-        CANCEL: "initial",
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: "search",
+            actions: "imprimirInicio",
+          },
+        },
       },
-    },
-    tickets: {
-      on: {
-        FINISH: "initial",
+      search: {
+        entry: "imprimirEntrada",
+        exit: "imprimirSalida",
+        on: {
+          CONTINUE: {
+            target: "passengers",
+            actions: assign({
+              selectedCountry: (context, event) => event.selectedCountry,
+            }),
+          },
+          CANCEL: "initial",
+        },
       },
-    },
-    passengers: {
-      on: {
-        DONE: "tickets",
-        CANCEL: "initial",
+      tickets: {
+        on: {
+          FINISH: "initial",
+        },
+      },
+      passengers: {
+        on: {
+          DONE: "tickets",
+          CANCEL: "initial",
+          ADD: {
+            target: "passengers",
+            actions: assign((context, event) =>
+              context.passengers.push(event.newPassenger)
+            ),
+          },
+        },
       },
     },
   },
-});
+  {
+    actions: {
+      imprimirInicio: () => console.log("Imprimir inicio"),
+      imprimirEntrada: () => console.log("Imprimir entrada a search"),
+      imprimirSalida: () => console.log("Imprimir salida del search"),
+    },
+  }
+);
 
 export default bookingMachine;
